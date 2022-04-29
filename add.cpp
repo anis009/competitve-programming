485b799a6f859e2265ceb95a519cd00350a06f24
MD.Anis Molla <41534160+anichu@users.noreply.github.com>
2022-04-29 23:39:22 +0600

    modified the template file

--- add.cpp
+++ add.cpp
@@ -6,17 +6,8 @@ int main()
 	// freopen("addin.txt","r",stdin);
 	// freopen("addout.txt","w",stdout);
 
-	long long int a=1,b,fib=1;
-	
-	cout<<1<<" "<<1<<" ";
-	
-	for(int i=0;i<100;i++){
-		b = fib;
-		fib = fib + a;
-		cout<<fib<<" ";
-		a = b;
-
-	}
+	long long  a= 0L;
+	cout<<a<<endl;
 
 
 	return 0;
